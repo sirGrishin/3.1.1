9e078d0012eea2f4e4779fb955863901bdc30373
sirGrishin <79722832+sirGrishin@users.noreply.github.com>
2021-04-19 14:22:16 +0200

    Add files via upload

---
+++ 3.1.1/2.2.2. spring_mvc/src/main/java/app/config/SecurityInitializer.java
@@ -0,0 +1,8 @@
+package app.config;
+
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+    //пустой класс, использующийся для регистрации модуля в спринг-контейнере
+}
