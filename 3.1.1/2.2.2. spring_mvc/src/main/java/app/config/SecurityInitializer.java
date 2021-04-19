f05de734e0832895ceeab3aae069de0a7e95f10d
sirGrishin <79722832+sirGrishin@users.noreply.github.com>
2021-04-19 21:56:57 +0200

    Delete SecurityInitializer.java

--- 3.1.1/2.2.2. spring_mvc/src/main/java/app/config/SecurityInitializer.java
+++
@@ -1,8 +0,0 @@
-package app.config;
-
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-    //пустой класс, использующийся для регистрации модуля в спринг-контейнере
-}
